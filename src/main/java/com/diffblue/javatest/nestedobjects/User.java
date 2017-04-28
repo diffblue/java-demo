79f205f513063fb2817b30989c7b8a561d373620
Robert (Jamie) Munro <jamie@diffblue.com>
2017-04-28 15:29:20 +0100

    Add some nested objects and packages

---
+++ src/main/java/com/diffblue/javatest/nestedobjects/User.java
@@ -0,0 +1,24 @@
+package com.diffblue.javatest.nestedobjects;
+
+// Copyright 2016-2017 DiffBlue limited. All rights reserved.
+
+import com.diffblue.javatest.nestedobjects.subpackage.Item;
+import com.diffblue.javatest.nestedobjects.subpackage.Order;
+
+public class User {
+  public User(Order order)  {
+    this.order = order;
+  }
+
+  /**
+   * Checks if an item costs the same as the user's order item.
+   */
+  public boolean checkItemCost(Item item) {
+    if (!order.hasItem()) {
+      return false;
+    }
+    return order.item.cost == item.cost;
+  }
+
+  private Order order;
+}
