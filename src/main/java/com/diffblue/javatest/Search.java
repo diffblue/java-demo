5e0610573e77b76fcf7e24d5ceb9c1d879be0413
Robert (Jamie) Munro <jamie@diffblue.com>
2017-04-28 15:15:19 +0100

    Simple method to find an integer in an array

---
+++ src/main/java/com/diffblue/javatest/Search.java
@@ -0,0 +1,21 @@
+package com.diffblue.javatest;
+
+// Copyright 2016-2017 DiffBlue limited. All rights reserved.
+
+public class Search {
+  /**
+   * Checks if an integer is contained within an array.
+   */
+  public boolean contains(int [] array, int target) {
+    boolean found = false;
+    int i;
+
+    for (i = 0; i < array.length; ++i) {
+      if (array[i] == target) {
+        found = true;
+      }
+    }
+
+    return found;
+  }
+}
