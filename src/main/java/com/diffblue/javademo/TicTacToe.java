c9caae9d82bf5c874e248c73f916295d5e0f385a
jgwilson42 <james.wilson@diffblue.com>
2018-02-18 19:09:13 +0000

    Add checkstyle and fix styling errors

--- src/main/java/com/diffblue/javademo/TicTacToe.java
+++ src/main/java/com/diffblue/javademo/TicTacToe.java
@@ -15,20 +15,20 @@ public class TicTacToe {
    *   1: player X has won
    *   2: player O has won
    */
-  public int checkTicTacToePosition(int[] a) {
+  public int checkTicTacToePosition(int[] board) {
     // First check number of cells
-    if (a.length != 9) {
+    if (board.length != 9) {
       return -1;
     }
 
     // Next check number of moves each player made
     int diff = 0;
     for (int i = 0; i < 9; i++) {
-      if (a[i] == 1) {
+      if (board[i] == 1) {
         diff++;
-      } else if (a[i] == 2) {
+      } else if (board[i] == 2) {
         diff--;
-      } else if (a[i] != 0) {
+      } else if (board[i] != 0) {
         return -1;
       }
     }
@@ -42,37 +42,37 @@ public class TicTacToe {
 
     // Check if a row and column has won
     for (int i = 0; i < 3; i++) {
-      if (a[i] == a[3 + i] && a[i] == a[6 + i]) {
-        if (a[i] == 1) {
+      if (board[i] == board[3 + i] && board[i] == board[6 + i]) {
+        if (board[i] == 1) {
           return 1;
-        } else if (a[i] == 2) {
+        } else if (board[i] == 2) {
           return 2;
         }
       }
 
-      if (a[3 * i] == a[3 * i + 1] && a[3 * i] == a[3 * i + 2]) {
-        if (a[3 * i] == 1) {
+      if (board[3 * i] == board[3 * i + 1] && board[3 * i] == board[3 * i + 2]) {
+        if (board[3 * i] == 1) {
           return 1;
-        } else if (a[3 * i] == 2) {
+        } else if (board[3 * i] == 2) {
           return 2;
         }
       }
     }
 
     // Check if the top-left bottom-right diagonal has won
-    if (a[0] == a[4] && a[0] == a[8]) {
-      if (a[0] == 1) {
+    if (board[0] == board[4] && board[0] == board[8]) {
+      if (board[0] == 1) {
         return 1;
-      } else if (a[0] == 2) {
+      } else if (board[0] == 2) {
         return 2;
       }
     }
 
     // Check if the top-right bottom-left diagonal has won
-    if (a[2] == a[4] && a[2] == a[6]) {
-      if (a[2] == 1) {
+    if (board[2] == board[4] && board[2] == board[6]) {
+      if (board[2] == 1) {
         return 1;
-      } else if (a[2] == 2) {
+      } else if (board[2] == 2) {
         return 2;
       }
     }
