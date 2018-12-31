321cf913ea2e7576851f371b0867512ddc0327af
James Adam <james.adam@diffblue.com>
2018-12-31 16:07:07 +0000

    Add sample function

--- src/main/java/com/diffblue/javademo/JMethods.java
+++ src/main/java/com/diffblue/javademo/JMethods.java
@@ -3,7 +3,7 @@ package main.java.com.diffblue.javademo;
 public class JMethods {
 
   // method defined
-  private static int getSquare(int X) {
-    return X * X;
+  private static int getSquare(int ax) {
+    return ax * ax;
   }
 }
