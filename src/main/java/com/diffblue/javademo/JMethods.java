44d507b53fb649e79bd77522bd6f103c0ce518ae
James Adam <james.adam@diffblue.com>
2018-12-31 16:02:26 +0000

    Add sample function

--- src/main/java/com/diffblue/javademo/JMethods.java
+++ src/main/java/com/diffblue/javademo/JMethods.java
@@ -6,12 +6,4 @@ public class JMethods {
     private static int getSquare(int x){
         return x * x;
     }
-
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-
-            // method call
-            result = getSquare(i);
-            System.out.println("Square of " + i + " is : " + result); }
-    }
 }
