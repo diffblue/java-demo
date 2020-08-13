6aacdbf971e5db4c0d9f35e0b4d2e07be80e29ca
Robert (Jamie) Munro <jamie@diffblue.com>
2020-08-13 09:50:52 +0000

    Add module with spaces in it's name
    for testing TG-12538

---
+++ module with spaces/src/main/java/com/diffblue/javademo/App.java
@@ -0,0 +1,14 @@
+package com.diffblue.javademo;
+
+/**
+ * Hello world.
+ */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+  public static Integer foo(Integer bar) {
+    return bar >= 10 ? bar + 1 : bar - 1;
+  }
+}
